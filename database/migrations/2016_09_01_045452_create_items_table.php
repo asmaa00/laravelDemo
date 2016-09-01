5ccf4cb60a61edd66fc0eac6fa8066e2850f0d11
asmaa00 <asmaa.elsayed00@gmail.com>
2016-09-01 07:29:06 +0200

    entrust middleware

--- database/migrations/2016_09_01_045452_create_items_table.php
+++ database/migrations/2016_09_01_045452_create_items_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGroupsTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,13 @@ class CreateGroupsTable extends Migration
      */
     public function up()
     {
-        Schema::create('groups', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
-        });
+        //
+        Schema::create('items', function (Blueprint $table) {
+           $table->increments('id');
+           $table->string('title');
+           $table->text('description');
+           $table->timestamps();
+       });
     }
 
     /**
@@ -26,6 +28,7 @@ class CreateGroupsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('groups');
+        //
+        Schema::drop("items");
     }
 }
